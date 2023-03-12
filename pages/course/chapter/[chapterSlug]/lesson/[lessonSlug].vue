feb99e1a60de8f34af97d9767687ff3408f4f690
Colin Schwarz <19342760+colinscz@users.noreply.github.com>
2023-03-12 13:22:51 +0100

    feat: 4-3 Inline, Named, and Global Route Middleware

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -31,31 +31,36 @@
 
 <script setup>
 const course = useCourse()
-
 const route = useRoute()
 
 definePageMeta({
-  middleware: function ({ params }, from) {
-    const course = useCourse()
-    const chapter = course.chapters.find(
-      (chapter) => chapter.slug === params.chapterSlug
-    )
-    if (!chapter) {
-      return abortNavigation({
-        statusCode: 404,
-        message: 'Chapter not found',
-      })
-    }
-    const lesson = chapter.lessons.find(
-      (lesson) => lesson.slug === params.lessonSlug
-    )
-    if (!lesson) {
-      return abortNavigation({
-        statusCode: 404,
-        message: 'Lesson not found',
-      })
-    }
-  },
+  middleware: [
+    function ({ params }, from) {
+      const course = useCourse()
+
+      const chapter = course.chapters.find(
+        (chapter) => chapter.slug === params.chapterSlug
+      )
+      if (!chapter) {
+        return abortNavigation({
+          statusCode: 404,
+          message: 'Chapter not found',
+        })
+      }
+
+      const lesson = chapter.lessons.find(
+        (lesson) => lesson.slug === params.lessonSlug
+      )
+
+      if (!lesson) {
+        return abortNavigation({
+          statusCode: 404,
+          message: 'Lesson not found',
+        })
+      }
+    },
+    'auth',
+  ],
 })
 
 const chapter = computed(() => {
