c96ca64ab52f9f26e35b621886bf67b87b8c67ee
Colin Schwarz <19342760+colinscz@users.noreply.github.com>
2023-04-15 15:37:02 +0200

    5-14 Update Course Meta Endpoint

--- types/course.ts
+++ types/course.ts
@@ -3,15 +3,3 @@ import { Lesson } from '@prisma/client'
 export type LessonWithPath = Lesson & {
   path: string
 }
-
-export type Chapter = {
-  title: string
-  slug: string
-  number: number
-  lessons: Lesson[] | LessonWithPath[]
-}
-
-export type Course = {
-  title: string
-  chapters: Chapter[]
-}
--- types/global.d.ts
+++ types/global.d.ts
@@ -3,22 +3,3 @@ export {}
 declare global {
   type Maybe<T> = T | null | undefined
 }
-
-type OutlineBase = {
-  title: string
-  slug: string
-  number: number
-}
-
-export type OutlineChapter = OutlineBase & {
-  lessons: OutlineLesson[]
-}
-
-export type OutlineLesson = OutlineBase & {
-  path: string
-}
-
-export type CourseMeta = {
-  title: string
-  chapters: OutlineChapter[]
-}
