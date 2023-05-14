2398ecc0cfd2e4a22053c28fa1a02a3bdfb25ed3
colinscz <19342760+colinscz@users.noreply.github.com>
2023-05-14 21:03:14 +0200

    7-9 Add the PaymentIntent Endpoint

--- components/Payment.vue
+++ components/Payment.vue
@@ -1,7 +1,21 @@
 <template>
   <Modal @close="$emit('close')">
     <div class="w-full max-w-2xl p-8 bg-slate-200 rounded-xl">
-      <form>
+      <div
+        v-if="success"
+        class="flex flex-col justify-center items-center space-y-6"
+      >
+        <h2 class="text-xl font-bold">
+          Thanks for buying the course!
+        </h2>
+        <button
+          @click="login"
+          class="mt-4 w-full text-md text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline flex items-center justify-center transition bg-blue-300 hover:bg-blue-200"
+        >
+          Login with Github to access
+        </button>
+      </div>
+      <form v-else @submit.prevent="handleSubmit">
         <h2 class="text-xl font-bold text-center">Buying {{ course.title }}</h2>
         <div class="px-8 py-6 mt-8 text-base bg-white rounded shadow-md width">
           <div class="flex items-center justify-between w-full mb-8">
@@ -22,9 +36,18 @@
         </div>
 
         <button
-          class="flex items-center justify-center w-full h-12 px-16 mt-4 font-sans text-lg font-bold text-black transition bg-yellow-300 rounded cursor-pointer focus:outline-none focus:shadow-outline hover:bg-yellow-200"
-        >
-          <div>Pay $97</div>
+        class="font-sans mt-4 w-full text-lg text-black h-12 px-16 rounded focus:outline-none focus:shadow-outline font-bold flex items-center justify-center transition"
+          :class="
+            processingPayment || email === ''
+              ? 'bg-gray-300 cursor-not-allowed'
+              : 'bg-yellow-300 hover:bg-yellow-200 cursor-pointer'
+          "
+          :disabled="processingPayment || email === ''">
+        <Loading
+            v-if="processingPayment"
+            class="h-5 w-5"
+          />
+          <div v-else>Pay $97</div>
         </button>
       </form>
     </div>
